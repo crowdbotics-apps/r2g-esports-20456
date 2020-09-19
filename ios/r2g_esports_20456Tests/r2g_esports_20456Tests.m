d5dcb781d1e87571507e3297d7ba81793f3070c8
Crowdbotics <team@crowdbotics.com>
2020-09-19 21:17:02 +0000

    Initial commit.

---
+++ ios/r2g_esports_20456Tests/r2g_esports_20456Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface r2g_esports_20456 : XCTestCase
+
+@end
+
+@implementation r2g_esports_20456
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
